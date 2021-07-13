1625714f10a8faab220599fb2d4c93d04b1aeaa2
Никита Ничепорук <tasher228227@gmail.com>
2021-07-13 22:08:29 +0300

    Add table

---
+++ UIPickerMonkeys/ImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewController.swift
+//  UIPickerMonkeys
+//
+//  Created by Никита Ничепорук on 7/13/21.
+//  Copyright © 2021 Никита Ничепорук. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- UIPickerMonkeys/ViewController.swift
+++ UIPickerMonkeys/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var monkeysTable: UITableView!
     var monkeysList: Array<String> = []
     let picker = UIPickerView()
+    var secVC: ImageViewController?
+    
+    let imageListMonkeys = ["https://givotniymir.ru/wp-content/uploads/2016/11/kapucin-obezyana-obraz-zhizni-i-sreda-obitaniya-obezyany-kapucin-7.jpg", "https://news.online.ua/proxy/news/r2-049315276a/280_5ad0afb902822.jpg", "https://upload.wikimedia.org/wikipedia/commons/d/de/Papio_cynocephalus02.jpg", "https://cdnimg.rg.ru/img/content/203/95/58/chimpanzee-3707270_1280_d_850.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Cebus_albifrons_edit.jpg/275px-Cebus_albifrons_edit.jpg", "https://static.wikia.nocookie.net/madagascar/images/a/ad/Julien-character-web-desktop.png/revision/latest/scale-to-width-down/250?cb=20210107113956&path-prefix=ru"]
     
     func toolBar() -> UIToolbar {
         let toolbar = UIToolbar()
@@ -25,8 +29,8 @@ class ViewController: UIViewController {
         return toolbar
     }
     
-   @objc func selectItem() {
-        textFieldMonkeys.text =  "\(picker.selectedRow(inComponent: 0) + 1) " + monkeysList[picker.selectedRow(inComponent: 1)]
+    @objc func selectItem() {
+        textFieldMonkeys.text =  "\(picker.selectedRow(inComponent: 0) + 1) " + monkeysList[picker.selectedRow(inComponent: 1)] //.0
         textFieldMonkeys.resignFirstResponder()
     }
     
@@ -38,6 +42,10 @@ class ViewController: UIViewController {
         picker.dataSource = self
         textFieldMonkeys.inputView = picker
         textFieldMonkeys.inputAccessoryView = toolBar()
+        monkeysTable.delegate = self
+        monkeysTable.dataSource = self
+        
+        secVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "secVC") as? ImageViewController
     }
 }
 
@@ -49,7 +57,7 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
         case 0:
-           return 10
+            return 10
         case 1:
             return monkeysList.count
         default:
@@ -62,9 +70,54 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         case 0:
             return "\(row + 1)"
         case 1:
-            return monkeysList[row]
+            return monkeysList[row]//.0
         default:
             return ""
         }
     }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return monkeysList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentName = monkeysList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = currentName
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let vc = secVC else {return}
+        _ = vc.view
+        DispatchQueue.global(qos: .background).async {
+            if let imageUrl = URL(string: self.imageListMonkeys[indexPath.row]),
+                let imageData = try? Data(contentsOf: imageUrl) {
+                let image = UIImage(data: imageData)
+                DispatchQueue.main.async {
+                    if image == nil {
+                        let alert = UIAlertController(title: "Error",
+                                                      message: "Picture not found",
+                                                      preferredStyle: .alert)
+                        let ok = UIAlertAction(title: "OK",
+                                               style: .default,
+                                               handler: nil)
+                        alert.addAction(ok)
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        vc.imageView.image = image
+                        self.navigationController?.show(vc, sender: nil)
+                    }
+                }
+            }
+            
+        }
+    }
+    
 }
